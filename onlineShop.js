7025328ffc12bcedbe668e9a692e975e9c163a6f
FatherofElias <supra_3_8@hotmail.com>
2024-10-14 11:37:51 -0500

    update

--- onlineShop.js
+++ onlineShop.js
@@ -1,7 +1,11 @@
-let loggedIn = true;
+let loggedIn = true; // Task 1 and 2
+let cart = ['Shirts', 'Shoes', 'Bread', 'Cell Phone', 'Candy Corn', 'The Pragmatic Programmer: Your Journey To Mastery, 20th Anniversary Edition (2nd Edition)', 'Tomato'];
 
 if (loggedIn) {
-  console.log("User is logged in. You can browse products.");
+  console.log("Here are the products in your cart:");
+  for (let i = 0; i < cart.length; i++) {
+    console.log(cart[i]);
+  }
 } else {
-  console.log("Please log in to browse products.");
+  console.log("Please log in to view your cart.");
 }
